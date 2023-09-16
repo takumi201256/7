e595c0d3e1f3be8a4d36f81d35a1ebaf97f7e39d
takumi201256 <takumi201651@gmail.com>
2023-09-16 15:20:25 +0900

    ブログ記 事作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,5 +13,8 @@ class PostController extends Controller
 }
 public function show(Post $post)
 { return view('posts/show')->with(['post' => $post]);}
+
+public function create()
+{ return view('posts/create');}
 }
 
